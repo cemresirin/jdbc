01c11bdfff4d47ee24d901f2b0b73c51f5c881d3
cemresirin <cemre25355@gmail.com>
2025-07-10 15:03:34 +0300

    pom.xml ve database config dosyası eklendi

---
+++ jdbc/src/main/java/com/cemre/config/DatabaseConfig.java
@@ -0,0 +1,9 @@
+package com.cemre.config;
+
+public class DatabaseConfig {
+
+    public static final String DATABASE_NAME="jdbc";
+    public static final String DATABASE_USERNAME="postgres";
+    public static final String DATABASE_PASSWORD="cemre";
+    public static final String DATABASE_URL="jdbc:postgresql://localhost:5432/"+DATABASE_NAME;
+}
