95de5e62670a2ba0d655d8085089396e67f00d47
cemresirin <cemre25355@gmail.com>
2025-07-10 19:09:56 +0300

    Tablo oluşturuldu

--- jdbc/src/main/java/com/cemre/Main.java
+++ jdbc/src/main/java/com/cemre/Main.java
@@ -2,10 +2,27 @@ package com.cemre;
 
 import com.cemre.config.DatabaseConnectorConfig;
 import java.sql.Connection;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
 
+        String sql = "CREATE TABLE IF NOT EXISTS users("+
+                "id SERIAL PRIMARY KEY," +
+                "name VARCHAR(100), " +
+                "email VARCHAR(100) " +
+                ")";
+
+        try{
+            Connection connection = DatabaseConnectorConfig.getConnection();
+            Statement statement = connection.createStatement();
+
+            statement.execute(sql);
+            System.out.println("Table created");
+
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
