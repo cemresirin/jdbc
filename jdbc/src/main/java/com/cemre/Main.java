e2a03e0868f8c259716af3fa8e2a71f656901a8b
cemresirin <cemre25355@gmail.com>
2025-07-13 18:26:10 +0300

    ResultSet ile veriler listelendi

--- jdbc/src/main/java/com/cemre/Main.java
+++ jdbc/src/main/java/com/cemre/Main.java
@@ -1,13 +1,13 @@
 package com.cemre;
 
 import com.cemre.config.DatabaseConnectorConfig;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
+
+/*
 
         String sql = "CREATE TABLE IF NOT EXISTS users(" +
                 "id SERIAL PRIMARY KEY," +
@@ -25,6 +25,8 @@ public class Main {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+ */
+
 
         String insertSql = "INSERT INTO users (name,email) VALUES(?,?)";
 
@@ -38,5 +40,23 @@ public class Main {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+        String selectsql = " SELECT * FROM users";
+
+        try {
+            Connection connection = DatabaseConnectorConfig.getConnection();
+            PreparedStatement preparedStatement =connection.prepareStatement(selectsql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                System.out.println("ID: " + resultSet.getInt("id"));
+                System.out.println("Name: " + resultSet.getString("name"));
+                System.out.println("Email: " + resultSet.getString("email"));
+
+            }
+
+            }catch(SQLException e){
+                throw new RuntimeException(e);
+            }
+        }
     }
-}
